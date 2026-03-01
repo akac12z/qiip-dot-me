0680a21f870c5fac9c45a8ed10b812e64adc6d3a
Chema <mrjark7@gmail.com>
2026-03-01 19:46:29 +0100

    changed toggle styles

--- src/components/ui/darkmode/ToggleTheme.tsx
+++ src/components/ui/darkmode/ToggleTheme.tsx
@@ -2,14 +2,14 @@ import { useRef, useState, useEffect } from "react";
 import styles from "./toggleTheme.module.css";
 
 const SunIcon = () => (
-  <svg width="18" height="18" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" aria-hidden="true" viewBox="0 0 24 24">
+  <svg className={styles.icon}  fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" aria-hidden="true" viewBox="0 0 24 24">
     <circle cx="12" cy="12" r="4"/>
     <path d="M12 2v3m0 14v3M4.22 4.22l2.12 2.12m11.32 11.32 2.12 2.12M2 12h3m14 0h3M4.22 19.78l2.12-2.12M17.66 6.34l2.12-2.12"/>
   </svg>
 );
 
 const MoonIcon = () => (
-  <svg width="18" height="18" fill="currentColor" stroke="none" aria-hidden="true" viewBox="0 0 24 24">
+  <svg className={styles.icon}  fill="currentColor" stroke="none" aria-hidden="true" viewBox="0 0 24 24">
     <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79"/>
   </svg>
 );
