d317d7de0d147378e72c3fb2e7aa3b60f4bde289
Chema <mrjark7@gmail.com>
2026-05-27 09:33:55 +0200

    created dotshape component and added styles

---
+++ src/features/qr/components/DotShape.tsx
@@ -0,0 +1,32 @@
+import styles from "./qr.module.css";
+
+import { DOT_LEVELS, type DOT_LEVEL } from "../rules/dot.ts";
+
+interface DotShapeProps {
+	DOT_LEVEL: DOT_LEVEL;
+	setDOT_LEVEL: React.Dispatch<React.SetStateAction<DOT_LEVEL>>;
+}
+
+export default function DotShape({ DOT_LEVEL, setDOT_LEVEL }: DotShapeProps) {
+	return (
+		<div className={styles.field}>
+			<span className={styles.label}>Dot correction level</span>
+			<div className={styles.errorToggleGroup}>
+				{DOT_LEVELS.map((level) => (
+					<button
+						key={level.value}
+						type="button"
+						className={`${styles.errorToggleBtn} ${DOT_LEVEL === level.value ? styles.errorToggleBtnActive : ""}`}
+						onClick={() => setDOT_LEVEL(level.value)}
+					>
+						<span className={styles.errorToggleName}>{level.label}</span>
+						<span className={styles.errorTogglePercent}>{level.percent}</span>
+					</button>
+				))}
+			</div>
+			<span className={styles.hint}>
+				Higher level = more damage resistance while printing
+			</span>
+		</div>
+	);
+}
